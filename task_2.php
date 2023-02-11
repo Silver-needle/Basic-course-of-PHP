fac1ee46ad3c40f054805aa427bd77fc0cbc1c1d
Samoray <msamoray161@gmail.com>
2023-02-11 17:45:04 +0300

    add task2

--- task_2.php
+++ task_2.php
@@ -1,3 +1,15 @@
 <?php
+$arrWishes = ['счастья', 'сна', 'достатка', 'отпуска', 'вдохновения', 'веселья'];
+$arrEpithets = ['бесстыдного', 'злостного', 'фееричного', 'всепоглощающего', 'безудержного'];
+$name = readline('Введите, пожалуйста, имя именинника   ');
 
-?>
+for ($i = 0; $i <= 2; $i++) {
+    $ranE = $arrEpithets[array_rand($arrEpithets)];
+    $ranW = $arrWishes[array_rand($arrWishes)];
+    $phrases[] = $ranE . ' ' . $ranW;
+    
+}
+//unset($ranE[$arrEpithets], $ranW[$arrWishes]);
+
+print_r($phrases);
+echo 'Дорогой(-ая), ' . $name . ', поздравляю тебя с днем рождения! Желаю ' . implode(', ', $phrases) . '!';
